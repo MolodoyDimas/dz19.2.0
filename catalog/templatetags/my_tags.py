352c71a60d96660868994325fc21f47fe32ef7b9
Dmitry <kip@gmail.com>
2023-11-18 16:53:47 +0700

    дз20.1 : Тег создан

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -7,3 +8,10 @@ def mymedia(x):
     if x:
         return f'/media/{str(x)}'
     return
+
+def mediapath(file_name):
+    if file_name:
+        return f'{settings.MEDIA_URL}{file_name}'
+    return ''
+# тег <img src="{% mediapath object.image %}" />
+# Но я незнаю куда нужно его запихать, т.к картинки работают через фильтр, или я что-то не понял
