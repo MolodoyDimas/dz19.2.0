5718ab5713b12016a5af5678d3dfa6f333b35b7e
Dmitry <kip@gmail.com>
2023-12-01 22:49:54 +0700

    ДЗ 22.1 : Редактирование

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,7 +6,7 @@ class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['class'] = 'form-group row'
 
 class ProductForm(StyleFormMixin, forms.ModelForm):
 
